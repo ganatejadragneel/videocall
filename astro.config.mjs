9b8fe70d8434c47c000d4e3530d291b801667d7d
Gana Teja Akula <ganatejaakula@Ganas-MacBook-Pro.local>
2024-08-12 12:26:09 -0700

    nit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from '@astrojs/node';
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: node({
-    mode: 'standalone' // or 'middleware'
+    mode: 'standalone'
   }),
+  vite: {
+    ssr: {
+      noExternal: ['agora-rtc-sdk-ng']
+    }
+  }
 });
