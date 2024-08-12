c969b1ca73db830ed451e20e4f64a7cf7f176f03
Gana Teja Akula <ganatejaakula@Ganas-MacBook-Pro.local>
2024-08-12 12:48:12 -0700

    nit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
 import react from "@astrojs/react";
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
